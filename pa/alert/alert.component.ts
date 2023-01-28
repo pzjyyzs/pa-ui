fde61308ac089ad68a331f0ecccc1a78d3583950
pzjyyzs <463669395@qq.com>
2023-01-28 20:57:48 +0800

    ui init

---
+++ pa/alert/alert.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+export type AlertType = 'success' | 'danger' | 'warning' | 'info';
+
+@Component({
+  selector: 'p-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.less']
+})
+export class AlertComponent implements OnInit {
+
+  @Input() type: AlertType = 'info';
+  // 是否显示图标，用于支持用户自定义图标
+  @Input() showIcon = true;
+  // 是否可关闭
+  @Input() closeable = false;
+
+  @Output() closeEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  hide = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  close() {
+    this.closeEvent.emit(true);
+    this.hide = true;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AlertModule } from '../../pa/alert/alert.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
