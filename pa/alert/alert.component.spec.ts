dd4c753c2b7b1289004027453079ebb6adb82f67
pzjyyzs <463669395@qq.com>
2023-04-17 23:32:11 +0800

    Alert 单元测试

--- pa/alert/alert.component.spec.ts
+++ pa/alert/alert.component.spec.ts
@@ -2,15 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
 
+
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
+      declarations: [AlertComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AlertComponent);
     component = fixture.componentInstance;
@@ -20,4 +21,17 @@ describe('AlertComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('alert type test', () => {
+
+    it('Alert should has info type', () => {
+      expect(fixture.elementRef.nativeElement.querySelector('.info')).not.toBe(null);
+    });
+
+    it('Alert should has success type', () => {
+      component.type = 'positive';
+      fixture.detectChanges();
+      expect(fixture.elementRef.nativeElement.querySelector('.positive')).not.toBe(null);
+    })
+  })
 });
